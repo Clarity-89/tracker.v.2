2beb04de33bc04ed2a078e143fc8b53542306c48
alex <homes89@ukr.net>
2016-05-11 17:23:45 +0300

    add integration test for user editing

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:test)
+  end
+
+  test "unsuccssful edit" do
+    log_in_as(@user, remember_me: '0')
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: {name: " ", email: "test@me", password: "11", password_confirmation: "111"}
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    log_in_as(@user, remember_me: '0')
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Test User"
+    email = "me@test.com"
+    patch user_path(@user), user: {name: name, email: email, password: "", password_confirmation: ""}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+end
