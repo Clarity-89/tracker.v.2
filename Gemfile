8b4dff9354ade71b045d37df554a8b1601dad285
alex <homes89@ukr.net>
2016-05-12 13:54:09 +0300

    Add pagination for users list + integration test

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 
-#Password encryption
-gem 'bcrypt', '3.1.7'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
@@ -35,7 +33,11 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+
+gem 'faker', '1.4.2'
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(name: "Alex K", email: "homes89@ukr.net", password: "111111", password_confirmation: "111111")
+
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
