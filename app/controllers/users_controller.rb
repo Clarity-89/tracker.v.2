5e66ab49269ef4b4e1cd77ab2cf0d426e8bb1da4
alex <homes89@ukr.net>
2016-05-12 12:14:47 +0300

    Add test for users redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 # noinspection Rails3Deprecated
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+
+  end
 
   def new
     @user = User.new
