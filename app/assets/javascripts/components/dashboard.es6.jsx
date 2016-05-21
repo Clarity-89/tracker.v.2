20c8a0166ebbae81f6f28bd82bec446b59a492f6
Clarity-89 <homes89@ukr.net>
2016-05-21 21:34:13 +0300

    Display search component

--- app/assets/javascripts/components/dashboard.es6.jsx
+++ app/assets/javascripts/components/dashboard.es6.jsx
@@ -1,9 +1,19 @@
 class Dashboard extends React.Component {
-    render () {
-        return (
-            <div>
-               
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+        console.log('got props', this.props)
+    }
+
+    render() {
+        return (<div className="row">
+            <div className="col s12 m4">
+                <Search />
             </div>
-        );
+        </div>);
     }
 }
+
--- app/assets/javascripts/components/search.es6.jsx
+++ app/assets/javascripts/components/search.es6.jsx
@@ -1,6 +1,9 @@
-class Search extends React.Component {
-  render () {
-    return <div />;
-  }
-}
+const Search = (props) => {
+    return <div className="input-field">
+        <input id="search" type="search"/>
+        <label for="search">Search</label>
+        <i className="material-icons">close</i>
+    </div>
+};
+
 
