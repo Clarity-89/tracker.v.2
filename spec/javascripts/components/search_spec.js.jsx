d8d37e6c61d256dab45e23292f75cce1bca6e0c3
alex <homes89@ukr.net>
2016-05-26 16:44:05 +0300

    Add first test forSearch component

--- spec/javascripts/components/search_spec.js.jsx
+++ spec/javascripts/components/search_spec.js.jsx
@@ -1,18 +1,19 @@
 var TestUtils = React.addons.TestUtils;
 
 describe('Search', function () {
+    var search = null, searchNode = null,
+        instance = null;
+    beforeEach(function () {
+        search = new Search({
+            value: 'testing',
+            changeHandler: jasmine.createSpy('changeHandler'),
+            clickHandler: jasmine.createSpy('clickHandler')
+        });
+        instance = TestUtils.renderIntoDocument(search);
+    });
 
     it('has default value set properly', function () {
-        var search = TestUtils.renderIntoDocument(
-            <div>
-                <Search value={'testing'} changeHandler={function(){}}
-                        clickHandler={function(){}}/>
-            </div>
-        );
-
-        var searchNode = ReactDOM.findDOMNode(search);
-        console.log('seach', search)
-
-        expect(searchNode.value).toEqual('testingg');
+        searchNode = ReactDOM.findDOMNode(instance).querySelectorAll("input")[0];
+        expect(searchNode.value).toEqual('testing');
     })
 });
