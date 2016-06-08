96840de349b1d93bf2b7e00f6c8b539440b9ca0a
Clarity-89 <homes89@ukr.net>
2016-06-08 10:30:09 +0300

    Remove mock users

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,3 @@ User.create!(name: "Example User",
              password: "foobar",
              password_confirmation: "foobar",
              activated: true, activated_at: Time.zone.now)
-
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true, activated_at: Time.zone.now)
-end
