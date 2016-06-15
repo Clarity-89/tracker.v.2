5ac7abba78f87b820464eec298e0a38583ba00ca
Clarity-89 <homes89@ukr.net>
2016-06-15 09:48:04 +0300

    Fix react component render issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require react
+//= require turbolinks
+// = require react
 //= require components
 //= require react_ujs
-//= require turbolinks
 //= require_tree .
 //= require jquery
 //= require jquery_ujs 
