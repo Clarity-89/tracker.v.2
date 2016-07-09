4ad630302b8b06a2d9fd734e0f127d6aeb65bc39
Clarity-89 <homes89@ukr.net>
2016-07-09 14:25:40 +0300

    email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
     # Prod Mailer config
     config.action_mailer.raise_delivery_errors = true
-    #config.action_mailer.delivery_method = :test
+    config.action_mailer.delivery_method = :sendmail
     host = 'trackerv2.herokuapp.com'
     config.action_mailer.default_url_options = {host: host, protocol: 'https'}
    # config.action_mailer.smtp_settings = { address: host }
