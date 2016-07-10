0e9fe198c6a1f6af909627859cf1574171764bdb
Clarity-89 <homes89@ukr.net>
2016-07-11 00:31:09 +0300

    Remove confirmation mail

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back user
-      else
-        message = "Account not activated."
-        message += " Check your email for the activation link."
-        flash[:waning] = message
-        redirect_to root_url
-      end
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+    def new
     end
-  end
 
-  def destroy
-    log_out if logged_in?
-    redirect_to root_url
-  end
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            # if user.activated?
+            log_in user
+            params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+            redirect_to serving_url
+        else
+            flash.now[:danger] = 'Invalid email/password combination'
+            render 'new'
+        end
+    end
+
+    def destroy
+        log_out if logged_in?
+        redirect_to root_url
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,75 @@
 # noinspection Rails3Deprecated
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy]
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: [:destroy]
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      @user.send_activation_email
-      flash[:info] = "please check your email to activate your account"
-      redirect_to root_url
-    else
-      render 'new'
+    def index
+        @users = User.paginate(page: params[:page])
     end
-  end
 
-  def edit
-
-  end
-
-  def update
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
+    def new
+        @user = User.new
     end
-  end
 
-  def destroy
-    puts 'deleting user', params[:id]
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # Confirm a logged-in user
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    def show
+        @user = User.find(params[:id])
     end
-  end
 
-  # Confirm the correct user
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in @user
+            flash[:success] = "Account created successfully"
+            redirect_to serving_url
+        else
+            render 'new'
+        end
+    end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def edit
+
+    end
+
+    def update
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        puts 'deleting user', params[:id]
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted"
+        redirect_to users_url
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Confirm a logged-in user
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+        end
+    end
+
+    # Confirm the correct user
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
+    end
 
 end
