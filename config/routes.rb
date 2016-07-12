9c0cb180c9fe86a2db09600734ee6149364b9292
Clarity-89 <homes89@ukr.net>
2016-07-12 19:39:35 +0300

    change home route

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,6 @@ Rails.application.routes.draw do
 
     # You can have the root of your site routed with "root"
     # root 'welcome#index'
-
     # Example of regular route:
     #   get 'products/:id' => 'catalog#view'
 
